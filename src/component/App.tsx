0544a0f1d5aa89fdd8c18567f373335633e914df
Nikhil Kumar <nikhil.kumar@publicissapient.com>
2019-09-29 11:36:51 +0530

    Initial commit

---
+++ src/component/App.tsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+export default class App extends Component {
+  
+  name:string = "awesome";
+ 
+  render() {
+ 
+    return (
+      <div>
+        <h1>Hi App</h1>
+      </div>
+    );
+  }
+}
