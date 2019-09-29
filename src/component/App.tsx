69fef14a4c24007977033142c22941707db8203c
Nikhil Kumar <nikhil.kumar@publicissapient.com>
2019-09-29 14:00:17 +0530

    checking hooks

--- src/component/App.tsx
+++ src/component/App.tsx
@@ -1,14 +1,13 @@
 import React, { Component } from "react";
 
 export default class App extends Component {
-  
-  name:string = "awesome";
- 
-  render() {
- 
+
+  public name: string = "awesome";
+  private age: number = 2;
+  public render() {
     return (
       <div>
-        <h1>Hi App</h1>
+        <h1>Hi App {this.name}, you are {this.age} years old</h1>
       </div>
     );
   }
