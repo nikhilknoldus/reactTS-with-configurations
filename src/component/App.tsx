6ef77d3a2f499870663501a87222bb8e9b828beb
Nikhil Kumar <nikhil.kumar@publicissapient.com>
2019-09-29 14:17:48 +0530

    testing configs

--- src/component/App.tsx
+++ src/component/App.tsx
@@ -2,13 +2,13 @@ import React, { Component } from "react";
 
 export default class App extends Component {
 
-  public name: string = "awesome";
-  private age: number = 2;
+  name: string = "awesome";
+  age: number = 2;
   public render() {
     return (
       <div>
         <h1>Hi App {this.name}, you are {this.age} years old</h1>
       </div>
     );
-  }
+   }
 }
